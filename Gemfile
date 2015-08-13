bafc317240e91bd55dd29acfd8d4dffa2de9413f
Ken Collins <ken@metaskills.net>
2015-08-12 20:18:01 -0400

    Start work.

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :test do
   gem 'simplecov'
   gem 'sinatra', '~> 1.3'
   gem 'typhoeus', '~> 0.3.3', :platforms => [:ruby_18, :ruby_19, :ruby_20, :ruby_21]
+  gem 'curb', '~> 0.8.6', :platforms => [:ruby_18, :ruby_19, :ruby_20, :ruby_21]
 end
 
 gemspec
---
+++ test/adapters/curb_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path('../integration', __FILE__)
+
+module Adapters
+  class Curb < Faraday::TestCase
+
+    def adapter() :curb end
+
+    Integration.apply(self) do
+
+      # # https://github.com/toland/patron/issues/34
+      # undef :test_PATCH_send_url_encoded_params
+
+      # # https://github.com/toland/patron/issues/52
+      # undef :test_GET_with_body
+
+      # # no support for SSL peer verification
+      # undef :test_GET_ssl_fails_with_bad_cert if ssl_mode?
+
+    end unless jruby?
+
+  end
+end
